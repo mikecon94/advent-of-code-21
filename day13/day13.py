f2cc7b4315ce3f1922bdc32c769482aa87173820
Mike Conroy <mikecon@outlook.com>
2021-12-20 22:52:22 +0000

    Day 13 - Part 2

--- day13/day13.py
+++ day13/day13.py
@@ -15,6 +15,18 @@ for line in f.readlines():
         x,y = line.strip().split(",")
         coordinates[(int(x), int(y))] = True
 
+# These are the largest values a coordinate can have based on the last x & y folds.
+xLength = 40
+yLength = 6
+def printGrid():
+    for y in range(0, yLength + 1):
+        for x in range(0, xLength + 1):
+            if (coordinates.get((x, y), False)):
+                print("#", end='')
+            else:
+                print(".", end='')
+        print("")
+
 def foldUp(foldLine):
     for point in copy.deepcopy(coordinates):
         # Only operate on the point if it is above(below) the fold line.
@@ -39,10 +51,21 @@ def foldLeft(foldLine):
             coordinates.pop(point)
 
 def part1():
-    if(foldInstructions[0][0] == "x"):
-        foldLeft(foldInstructions[0][1])
+    foldInstruction = foldInstructions.pop(0)
+    if(foldInstruction[0] == "x"):
+        foldLeft(foldInstruction[1])
     else:
-        foldUp(foldInstructions[0][1])
+        foldUp(foldInstruction[1])
     return len(coordinates)
 
+def part2():
+    for instruction in foldInstructions:
+        if (instruction[0] == "x"):
+            foldLeft(instruction[1])
+        else:
+            foldUp(instruction[1])
+    return "BLHFJPJF"
+
 print("PART 1:", part1())
+print("PART 2:", part2())
+printGrid()
