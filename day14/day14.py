1411d853f8e2b1e0a29d9f6aa74bb79dda5640c8
Mike Conroy <mconroy@mulesoft.com>
2021-12-21 15:41:09 +0000

    Add Recursive solution.

--- day14/day14.py
+++ day14/day14.py
@@ -41,4 +41,50 @@ def runPairInsertions(noOfIterations):
         iterationCount += 1
     return calculateScore(polymerTemplate)
 
+# This recursive method gets the correct result but is just as slow as part 1.
+# This is DFS whilst the method used for Part 1 is BFS.
+# It requires some preparation - e.g. to call with NNCB.
+#   charValues["N"] = 2
+#   charValues["C"] = 1
+#   charValues["B"] = 1
+#   calculate("NN", "C", 0, 40, charValues)
+#   calculate("NC", "B", 0, 40, charValues)
+#   calculate("CB", "H", 0, 40, charValues)
+
+# Visited Pairs:
+# Dictionary of pairs to track values for previously visited pairs.
+# NN -> {
+#           1 -> {
+#                   Values: {B: 2, N: 1}
+#                   String: {"NBB"}
+#                }
+#       }
+visitedPairs = dict()
+def calculate(pair, insertedChar, currentIteration, iterationLimit, charValues):
+    if(currentIteration == iterationLimit):
+        print(charValues)
+        return pair
+    else:
+        charValues[insertedChar] = charValues.get(insertedChar, 0) + 1
+        newPair1 = pair[0] + insertedChar
+        newPair2 = insertedChar + pair[1]
+        # Memoization
+        # If we have already calculated a pair for a certain number of iterations then update those values instead.
+
+
+        return calculate(newPair1, insertionRules[newPair1], currentIteration+1, iterationLimit, charValues) \
+                + calculate(newPair2, insertionRules[newPair2],currentIteration+1, iterationLimit, charValues)
+
+def part2():
+    charValues = dict()
+    charValues["N"] = 2
+    charValues["C"] = 1
+    charValues["B"] = 1
+    calculate("NN", "C", 0, 40, charValues)
+    calculate("NC", "B", 0, 40, charValues)
+    calculate("CB", "H", 0, 40, charValues)
+    print(charValues)
+    return 0
+
 print("PART 1:", runPairInsertions(10))
+print("PART 2:", part2())
